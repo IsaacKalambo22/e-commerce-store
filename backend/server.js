115f3359c799efa560cd5e2964b73fc0899cadb6
bed-com-43-19 <bed-com-43-19@unima.ac.mw>
2024-10-29 09:23:20 +0200

    Created a coupon and added it to the server

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js"
 import productRoutes from "./routes/product.route.js"
 import cartRoutes from "./routes/cart.route.js"
+import couponRoutes from "./routes/coupon.route.js"
 
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
@@ -24,6 +25,7 @@ app.use(cookieParser()); // To Access cookies
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
+app.use("/api/coupons", couponRoutes);
 
 // Start server
 app.listen(PORT, () => {
